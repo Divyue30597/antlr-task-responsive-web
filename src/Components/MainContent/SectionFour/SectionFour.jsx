fc72580b2294a9db7e2e73e948e582827d2a2346
Divyue30597 <tomchi.sharma305@outlook.com>
2023-04-12 22:22:17 +0530

    added responsiveness to the pages and some modification is done as well, included css variables

--- src/Components/MainContent/SectionFour/SectionFour.jsx
+++ src/Components/MainContent/SectionFour/SectionFour.jsx
@@ -5,22 +5,24 @@ export function SectionFour() {
   return (
     <div className="section-four">
       <div className="section-four-bg"></div>
-      <div className="container section-four-container">
-        <div className="section-four-image">
-          <figure>
-            <img
-              src={sectionFourImage}
-              alt="two circles and square and one ellipse"
-            />
-          </figure>
-        </div>
-        <div className="section-four-body">
-          <h1>Everything your business needs.</h1>
-          <p>
-            Our community is a collaboration hub, where the best tech people and
-            the right companies come together, helping each other to be the very
-            best at business.
-          </p>
+      <div className="container">
+        <div className="section-four-container">
+          <div className="section-four-image">
+            <figure>
+              <img
+                src={sectionFourImage}
+                alt="two circles and square and one ellipse"
+              />
+            </figure>
+          </div>
+          <div className="section-four-body">
+            <h1>Everything your business needs.</h1>
+            <p>
+              Our community is a collaboration hub, where the best tech people
+              and the right companies come together, helping each other to be
+              the very best at business.
+            </p>
+          </div>
         </div>
       </div>
     </div>
